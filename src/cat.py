b954adae7c50e6a3885e57e7a344abf9d8425f7b
audrbar <audrbar@gmail.com>
2024-05-22 20:18:39 +0300

    Upd: cat.py

--- src/cat.py
+++ src/cat.py
@@ -5,10 +5,10 @@ class Cat:
         self.color = color
         Cat.total_cats += 1
     def print_amount(self):
-        print(f'Sukurta {total_cats} kaciu.')
+        print(self.total_cats)
 
 cat1 = Cat('lola', 'green')
 cat2 = Cat('big', 'black')
 cat3 = Cat('red', 'red')
 print(f'Sukurtos kates: {cat1.name}, {cat2.name}, {cat3.name}')
-print(f'Sukurta {Cat.total_cats} kaciu.')
+print(f'Sukurtos {Cat.total_cats} kates.')
